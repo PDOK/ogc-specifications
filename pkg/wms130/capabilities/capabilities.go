25ecee5e7ce6b31021099eaa3b87857baa7f2ad2
Paul Verhoeven <paul.verhoeven@kadaster.nl>
2020-11-27 11:55:54 +0100

    Revert "Fixed LF->CRLF"
    This reverts commit fba024ce

--- pkg/wms130/capabilities/capabilities.go
+++ pkg/wms130/capabilities/capabilities.go
@@ -56,8 +56,8 @@ type Request struct {
 }
 
 // Exception struct containing the different available exceptions, should be filled from the template
-// Default: XML
-// Other commonly used: BLANK, INIMAGE and JSON
+// default: XML
+// other commonly used: BLANK, INIMAGE and JSON
 type Exception struct {
 	Format []string `xml:"Format" yaml:"format"`
 }
