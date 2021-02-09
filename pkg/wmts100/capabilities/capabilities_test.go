c39ad1b261ed619028a42435205d26138ded4157
Wouter Remijn <wouter.remijn@kadaster.nl>
2021-02-09 09:48:46 +0100

    added test for when legendurl is not added

--- pkg/wmts100/capabilities/capabilities_test.go
+++ pkg/wmts100/capabilities/capabilities_test.go
@@ -16,7 +16,7 @@ func bp(b bool) *bool {
 }
 
 // Example based on 7.1.1.3 ServiceMetadata document example from OGC WMTS 1.0.0 spec
-var contents = Contents{
+var contentsWithLegend = Contents{
 	Layer: []Layer{
 		{
 			Title: "etopo2",
@@ -49,13 +49,52 @@ var contents = Contents{
 	},
 }
 
-func TestBuildStyle(t *testing.T) {
+func TestBuildStyleWithLegend(t *testing.T) {
 	expected := `<Style isDefault="true">
   <ows:Identifier>default</ows:Identifier>
   <ows:Title>default</ows:Title>
   <LegendURL format="image/png" xlink:href="http://www.maps.bob/etopo2/legend.png"></LegendURL>
 </Style>`
-	output, _ := xml.MarshalIndent(contents.Layer[0].Style, "", "  ")
+	output, _ := xml.MarshalIndent(contentsWithLegend.Layer[0].Style, "", "  ")
+	if string(output) != expected {
+		t.Errorf("test: %d, expected: %s ,\n got: %s", 1, expected, string(output))
+	}
+}
+
+// Example based on 7.1.1.3 ServiceMetadata document example from OGC WMTS 1.0.0 spec
+var contentsWithoutLegend = Contents{
+	Layer: []Layer{
+		{
+			Title: "etopo2",
+			Abstract: `ETOPO2 - 2 minute Worldwide Bathymetry/Topography
+		Data taken from National Geophysical Data Center(NGDC),
+		ETOPO2 Global 2' Elevations, September 2001...`,
+			WGS84BoundingBox: ows.BoundingBox{LowerCorner: ows.Position{-180, -90}, UpperCorner: ows.Position{180, 90}},
+			Identifier:       "etopo2",
+			Metadata:         Metadata{Href: "http://www.maps.bob/etopo2/ metadata.htm"},
+			Style: []Style{
+				{
+					Title:      sp(`default`),
+					Identifier: `default`,
+					IsDefault: bp(true),
+				},
+			},
+			Format: "image/png",
+			TileMatrixSetLink: []TileMatrixSetLink{
+				{
+					TileMatrixSet: "WholeWorld_CRS_84",
+				},
+			},
+		},
+	},
+}
+
+func TestBuildStyleWithoutLegend(t *testing.T) {
+	expected := `<Style isDefault="true">
+  <ows:Identifier>default</ows:Identifier>
+  <ows:Title>default</ows:Title>
+</Style>`
+	output, _ := xml.MarshalIndent(contentsWithoutLegend.Layer[0].Style, "", "  ")
 	if string(output) != expected {
 		t.Errorf("test: %d, expected: %s ,\n got: %s", 1, expected, string(output))
 	}
