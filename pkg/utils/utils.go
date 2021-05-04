2d2e61134ccf0a004d079312c6eaba43e107e521
Wouter <wouter.visscher@gmail.com>
2021-05-04 19:53:30 +0200

    added Identify func() for GET & POST

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,8 +1,15 @@
 package utils
 
 import (
+	"encoding/xml"
 	"net/url"
 	"strings"
+
+	"github.com/pdok/ogc-specifications/pkg/common"
+)
+
+const (
+	REQUEST = `REQUEST`
 )
 
 // KeysToUpper convert all the keys to UpperCase
@@ -21,4 +28,26 @@ func KeysToLower(q url.Values) url.Values {
 		r[strings.ToLower(k)] = append(r[strings.ToLower(k)], v...)
 	}
 	return r
+}
+
+type identify struct {
+	XMLName xml.Name
+}
+
+func IdentifyRequest(doc []byte) (string, common.Exceptions) {
+	var i identify
+
+	if err := xml.Unmarshal(doc, &i); err != nil {
+		return ``, common.Exceptions{common.MissingParameterValue()}
+	} else {
+		return i.XMLName.Local, nil
+	}
+}
+
+func IdentifyRequestKVP(query url.Values) (string, common.Exceptions) {
+	if query[REQUEST] != nil {
+		return query[REQUEST][0], nil
+	}
+
+	return ``, common.Exceptions{common.MissingParameterValue()}
 }
--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"net/url"
 	"testing"
+
+	"github.com/pdok/ogc-specifications/pkg/common"
 )
 
 func TestKeysToUpper(t *testing.T) {
@@ -26,6 +28,61 @@ func TestKeysToUpper(t *testing.T) {
 		q := KeysToUpper(tq.query)
 		if len(q) != len(tq.expectedQuery) {
 			t.Errorf("test: %d, expected: %s \ngot: %s", k, tq.expectedQuery, q)
+		}
+	}
+}
+
+func TestIdentifyRequest(t *testing.T) {
+	var tests = []struct {
+		doc     []byte
+		request string
+		errors  common.Exceptions
+	}{
+		0: {doc: []byte(`<Mekker/>`), request: `Mekker`},
+		1: {doc: []byte(`<GetCapabilities/>`), request: `GetCapabilities`},
+		2: {doc: []byte(`</>`), errors: common.Exceptions{common.MissingParameterValue()}},
+		3: {doc: []byte(`<|\/|>`), errors: common.Exceptions{common.MissingParameterValue()}},
+		4: {doc: nil, errors: common.Exceptions{common.MissingParameterValue()}},
+	}
+
+	for k, i := range tests {
+		request, errs := IdentifyRequest(i.doc)
+		if errs != nil {
+			if errs[0].Error() != i.errors[0].Error() {
+				t.Errorf("test: %d, expected: %s \ngot: %s", k, i.errors[0].Error(), errs[0].Error())
+			}
+		} else {
+			if request != i.request {
+				t.Errorf("test: %d, expected: %s \ngot: %s", k, i.request, request)
+			}
+		}
+
+	}
+}
+
+func TestIdentifyRequestKVP(t *testing.T) {
+	var tests = []struct {
+		url     map[string][]string
+		request string
+		errors  common.Exceptions
+	}{
+		0: {url: map[string][]string{REQUEST: {`Mekker`}}, request: `Mekker`},
+		1: {url: map[string][]string{REQUEST: {`GetCapabilities`}}, request: `GetCapabilities`},
+		2: {url: map[string][]string{`SERVICE`: {`NoREQUESTKey`}}, errors: common.Exceptions{common.MissingParameterValue()}},
+		3: {url: map[string][]string{}, errors: common.Exceptions{common.MissingParameterValue()}},
+		4: {url: nil, errors: common.Exceptions{common.MissingParameterValue()}},
+	}
+
+	for k, i := range tests {
+		request, errs := IdentifyRequestKVP(i.url)
+		if errs != nil {
+			if errs[0].Error() != i.errors[0].Error() {
+				t.Errorf("test: %d, expected: %s \ngot: %s", k, i.errors[0].Error(), errs[0].Error())
+			}
+		} else {
+			if request != i.request {
+				t.Errorf("test: %d, expected: %s \ngot: %s", k, i.request, request)
+			}
 		}
 	}
 }
