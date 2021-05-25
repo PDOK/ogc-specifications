b68f6b78288158791a7c41a714cd9a3338264c1f
huisma1 <Boaz.Huisman@kadaster.nl>
2021-05-25 15:44:23 +0200

    Delete OperationRequest kvp

--- pkg/wfs200/operation_request_kvp.go
+++
@@ -1,9 +0,0 @@
-package wfs200
-
-import "net/url"
-
-type OperationRequestKVP interface {
-	ParseQueryParameters(url.Values) Exceptions
-	ParseOperationRequest(OperationRequest) Exceptions
-	ToQueryParameters() url.Values
-}
